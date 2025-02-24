2f962585922b501b4fe04f39fd2d8c2ad549022b
Guru901 <avinashkaur842@gmail.com>
2025-02-24 15:22:42 +0530

    added prompt function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::Stdin;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        prompt();
+    }
 }
+fn prompt() {
+    print!("$ ");
+    io::stdout().flush().unwrap();
+    let stdin: Stdin = io::stdin();
+    let mut input: String = String::new();
+    stdin.read_line(&mut input).unwrap();
+    let command: &str = input.trim();
+    handle_command(command);
+}
+
+fn handle_command(command: &str) {}
