81484da6064c0ae67d7d7079bbe4cb375aa43aa1
Guru901 <avinashkaur842@gmail.com>
2025-02-24 16:43:34 +0530

    add function to retrieve command arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 pub fn run_exit_command() {
     std::process::exit(0);
 }
+
+pub fn get_arguments_of_command(command: &str) -> Vec<&str> {
+    let mut inp: Vec<&str> = command.split(" ").collect();
+    inp.remove(0);
+
+    return inp;
+}
